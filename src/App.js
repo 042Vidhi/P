e80e44e8aaa47a0d6010ba85a74fd2a172a3a67b
042Vidhi <98405549+042Vidhi@users.noreply.github.com>
2023-02-24 23:48:28 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { BrowserRouter,Switch,Route } from 'react-router-dom';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Home from './Components/Home/Home';
+import ProjectIdeas from './Components/ProjectIdeas/ProjectIdeas';
+import FAQs from './Components/FAQs/FAQs';
+import Create from './Components/Create/Create';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Sidebar/>
+        <Switch>
+          <Route exact path="/home">
+            <Home/>
+          </Route>
+          <Route path="/ideas">
+            <ProjectIdeas/>
+          </Route>
+          <Route path="/faqs">
+            <FAQs/>
+          </Route>
+          <Route path="/create">
+            <Create/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
